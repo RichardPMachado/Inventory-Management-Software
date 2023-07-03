326b4d0d5feeadb639c6efc45fe41a9d71a8ae26
RichardPMachado <richardmachado2211@gmail.com>
2023-07-03 17:41:57 -0300

    feat[Seeders]: seed User implemented

---
+++ app/backend/src/data/userSeed.ts
@@ -0,0 +1,37 @@
+export const userSeed = [
+  {
+    email: 'richard@email.com',
+    name: 'Richard',
+    password: '$2b$10$NiLt4TsvBYg4VWcCCpDBluqi71INS1oKYKVp64lBMJjqiauVBKB0a', // "Pass#$wordKey123"
+    // "Pass#$wordKey123"
+    role: 'root',
+  },
+  {
+    email: 'gtardin@email.com',
+    name: 'Gustavo Tardin',
+    password: '$2b$10$R9hT87JnOpR1USTX3gK7pe2fBRK..q04dlAn6tDMHEQd4.iAf04zO',
+    // "Pass#$wordKey123"
+    role: 'admin',
+  },
+  {
+    email: 'test@email.com',
+    name: 'test',
+    password: '$2b$10$ZnCM4YaUnAE.rNDwftvWre5vocsb8yW/.qr4hc/11RywG89ndByTu',
+    // 'Pass@12345Test',
+    role: 'user',
+  },
+  {
+    email: 'test2@email.com',
+    name: 'test2',
+    password: '$2b$10$uOwhJZTr1NdfFWgtQ5nEAOpFGxagHWXZfG9Nta0eAirtPSGZZDW5u',
+    // 'Pass@12345Test',
+    role: 'user',
+  },
+  {
+    email: 'test3@email.com',
+    name: 'test3',
+    password: '$2b$10$jFhjRUz7pRCNHTVFz5dcI.sz9gNDobFk9lSKzJ5z4jG/OtyrND42.',
+    // 'Pass@12345Test',
+    role: 'user',
+  },
+];
