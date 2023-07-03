86735cdadf52b7b3cf2e34fbbacdb59880263d95
RichardPMachado <richardmachado2211@gmail.com>
2023-07-03 19:11:45 -0300

    feat[delete user]: delete user has been implemented

--- app/backend/src/user/user.controller.ts
+++ app/backend/src/user/user.controller.ts
@@ -6,6 +6,8 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -13,12 +15,15 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { CurrentUser } from '@/decorators/current-user.decorator';
 import { UpdatePasswordDto } from './dto/update-password.dto';
+import { IsPublic } from '@/decorators/is-public.decorator';
+import { Roles } from '@/decorators/roles-key.decorator';
+import { UserType } from '@/enum/user-type.enum';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // @IsPublic()
+  @IsPublic()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
@@ -29,6 +34,7 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @Roles(UserType.Root, UserType.Admin)
   @Get('id/:id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
@@ -61,8 +67,15 @@ export class UserController {
     return this.userService.updateUser(email, updateUserDto, currentUser);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Delete('id/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  removeById(@Param('id') id: string) {
+    return this.userService.removeById(+id);
+  }
+
+  @Delete('email/:email')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  removeByEmail(@Param('email') email: string) {
+    return this.userService.removeByEmail(email);
   }
 }
