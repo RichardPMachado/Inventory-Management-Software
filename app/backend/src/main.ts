35db9fead38fafdbfaf19d5b84c99a2b73f354e2
RichardPMachado <richardmachado2211@gmail.com>
2023-07-05 17:07:18 -0300

    feat[Main]: ValidationPipe was implemnted

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,8 +1,27 @@
+import 'dotenv/config';
+
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.APP_PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors();
+
+  await app.listen(PORT, () =>
+    Logger.log(`Server running on port ${PORT}`, 'Bootstrap'),
+  );
 }
+
 bootstrap();
