36775ec8f02890b985bc359ee89487b421cd1796
RichardPMachado <richardmachado2211@gmail.com>
2023-07-06 16:23:16 -0300

    feat[Auth]: authModule providers local Strategy and Jwt Strategy were implemented

--- app/backend/src/auth/auth.module.ts
+++ app/backend/src/auth/auth.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '@/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { LoginValidationMiddleware } from './middleware/login-validation.middleware';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'jwt_secret',
+      signOptions: { expiresIn: '30' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  }
+}
